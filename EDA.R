16b2c1c9acdc177f8ac59788a1317f8b0be08dda
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2022-04-01 09:48:45 -0400

    updated packages

--- EDA.R
+++ EDA.R
@@ -1,4 +1,5 @@
-pacman::p_load(readxl)
+pacman::p_load(readxl, tidyverse, Rfssa, plotly, fda,
+               cranly, ggplot2)
 
 discrete <- read_excel("Data/discrete.xls")
 IDinfo <- read_excel("Data/IDinfo.xls")
