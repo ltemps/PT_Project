b8e082c64fd6459168e917f12a2bd109a7ef5627
sisitzky <90338095+sisitzky@users.noreply.github.com>
2022-04-02 18:29:56 -0400

    Adding final 2 plots into Recreating_Plots

--- Recreating_Plots.R
+++ Recreating_Plots.R
@@ -1,11 +1,9 @@
 library(reshape2)
 library(ggplot2)
 library(tidyverse)
+library(circlize)
 
-#ML_GRF_stance_N <- read.csv("ML_GRF_stance_N.csv", header = FALSE)
-#AP_GRF_tn <- read.csv("AP_GRF_stance_N.csv", header = FALSE)
-
-
+## V ##
 V_GRF_stance_N <- read.csv("V_GRF_stance_N.csv", header = FALSE)
 
 # Transpose data & Add time %
@@ -14,7 +12,6 @@ V_GRF_stance_N <- data.frame(V_GRF_stance_N)
 V_GRF_stance_N$Time <- seq(1, 100, 1)
 
 # Plotting all obs
-library(circlize)
 mycolors <- rand_color(15696)
 plot_V_GRF <- melt(V_GRF_stance_N,  id.vars = 'Time', variable.name = 'V_GRF_stance')
 ggplot(plot_V_GRF, aes(Time, value, group = V_GRF_stance, color = V_GRF_stance)) + 
@@ -30,6 +27,56 @@ avg_plot_V_GRF <- plot_V_GRF %>%
                       summarise(avg_value = mean(value))
 ggplot(avg_plot_V_GRF, aes(Time, avg_value)) + geom_line()
 
+#-----------------------------------------------
+
+## ML ##
+
+ML_GRF_stance_N <- read.csv("ML_GRF_stance_N.csv", header = FALSE)
+
+# Transpose data & Add time %
+ML_GRF_stance_N <- t(ML_GRF_stance_N)
+ML_GRF_stance_N <- data.frame(ML_GRF_stance_N)
+ML_GRF_stance_N$Time <- seq(1, 100, 1)
+
+# Plotting all obs
+mycolors <- rand_color(15696)
+plot_ML_GRF <- melt(ML_GRF_stance_N,  id.vars = 'Time', variable.name = 'ML_GRF_stance')
+ggplot(plot_ML_GRF, aes(Time, value, group = ML_GRF_stance, color = ML_GRF_stance)) + 
+  geom_line() +
+  scale_colour_manual(values = mycolors) +
+  theme(legend.position = "none")
 
 
 
+# Plotting avg per time point
+avg_plot_ML_GRF <- plot_ML_GRF %>%
+  group_by(Time) %>%
+  summarise(avg_value = mean(value))
+ggplot(avg_plot_ML_GRF, aes(Time, avg_value)) + geom_line()
+
+#-----------------------------------------------
+
+## AP ##
+
+AP_GRF_stance_N <- read.csv("AP_GRF_stance_N.csv", header = FALSE)
+
+# Transpose data & Add time %
+AP_GRF_stance_N <- t(AP_GRF_stance_N)
+AP_GRF_stance_N <- data.frame(AP_GRF_stance_N)
+AP_GRF_stance_N$Time <- seq(1, 100, 1)
+
+# Plotting all obs
+mycolors <- rand_color(15696)
+plot_AP_GRF <- melt(AP_GRF_stance_N,  id.vars = 'Time', variable.name = 'AP_GRF_stance')
+ggplot(plot_AP_GRF, aes(Time, value, group = AP_GRF_stance, color = AP_GRF_stance)) + 
+  geom_line() +
+  scale_colour_manual(values = mycolors) +
+  theme(legend.position = "none")
+
+
+
+# Plotting avg per time point
+avg_plot_AP_GRF <- plot_AP_GRF %>%
+  group_by(Time) %>%
+  summarise(avg_value = mean(value))
+ggplot(avg_plot_AP_GRF, aes(Time, avg_value)) + geom_line()
