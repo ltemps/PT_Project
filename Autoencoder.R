8ffa0c5fc8eea5773e075cba2d2603e9609a4fa7
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2022-04-15 18:05:06 -0400

    attempt to start autoencoder

---
+++ Autoencoder.R
@@ -0,0 +1,40 @@
+#Autoencoder
+source(file= "fPCA.R")
+pacman::p_load(keras)
+#https://www.r-bloggers.com/2018/07/pca-vs-autoencoders-for-dimensionality-reduction/
+
+#train test split
+split1<- sample(c(rep(0, 0.7 * nrow(fda_V_tibble)), 
+                  rep(1, 0.3 * nrow(fda_V_tibble))))
+head(split1)
+x_train <- fda_V_tibble[split1 == 0, ]
+x_train <- as.matrix(x_train)
+
+#set model
+#units, activation currently chosen at random
+model <- keras_model_sequential()
+model %>% 
+  layer_dense(units = 10, activation = "relu", input_shape = ncol(x_train)) %>%
+  layer_dense(units = 6, activation = "relu", name = "bottleneck") %>%
+  layer_dense(units = 10, activation = "relu") %>%
+  layer_dense(units = ncol(x_train))
+#view layers
+summary(model)
+
+#compile model
+model %>%compile(
+  loss = "mean_squared_error",
+  optimizer = "adam"
+)
+
+#fit model (something here breaks)
+model %>% fit(
+  x = x_train, 
+  y = x_train,
+  epochs = 2000,
+  verbose = 0
+)
+
+#evaluate the performance of the model
+mse.ae2 <- evaluate(model, x_train, x_train)
+mse.ae2
