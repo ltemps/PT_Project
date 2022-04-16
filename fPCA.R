1c4db19b22b44ad998e889c3b3c65a399e64ce59
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2022-04-16 14:13:46 -0400

    repeated with rest of the time normalized data sets

--- fPCA.R
+++ fPCA.R
@@ -3,6 +3,7 @@ library(fda)
 library(tidyverse)
 library(plotly)
 
+##--------------------------------V_GRF Data
 fda_V_tibble <- as_tibble(plot_V_GRF)
 
 n_points <- 100
@@ -66,4 +67,132 @@ fun_pca$varprop #a vector giving the proportion of variance explained by each ei
 sum(fun_pca$varprop) #explains 91% of the variance
 fun_pca$scores[1:3,] #a matrix of scores on the principal components or harmonics, there are 15967 values, one for each curve
 
-#see if we can transition it into an autoencoder
+
+##--------------------------------ML_GRF_Data
+fda_ML_tibble <- as_tibble(plot_ML_GRF)
+
+n_points <- 100
+n_curves <- 15697 #number of observations
+max_time <- 100
+
+# setting knots & plotting basis
+knots = c(seq(0, max_time, 5)) #Location of knots
+n_knots = length(knots) #Number of knots
+n_order = 4 # order of basis functions: for cubic b-splines: order = 3 + 1
+n_basis   = length(knots) + n_order - 2;
+basis = create.bspline.basis(rangeval = c(0, max_time), n_basis)
+
+plot(basis) #currently using 20 knots, could try another number of knots to see if we obtain better variance
+
+# creating matrix for Time & value
+argvals <- matrix(fda_ML_tibble$Time, nrow = n_points, ncol = n_curves)
+y_mat <- matrix(fda_ML_tibble$value, nrow = n_points, ncol = n_curves)
+
+# creating object
+W.obj <- Data2fd(argvals = argvals, y = y_mat, basisobj = basis, lambda = 0.5)
+
+# finding mean & sd
+W_mean <- mean.fd(W.obj)
+W_sd <- std.fd(W.obj)
+
+# finding upper and lower SE
+SE_u <- fd(basisobj = basis)
+SE_l <- fd(basisobj = basis)
+
+# inputting SE's
+SE_u$coefs <- W_mean$coefs +  1.96 * W_sd$coefs/sqrt(n_curves) 
+SE_l$coefs <- W_mean$coefs -  1.96 * W_sd$coefs/sqrt(n_curves)
+
+# plot 1
+plot(W.obj, xlab="Time", ylab="", lty = 1)
+
+title(main = "Smoothed Curves")
+lines(SE_u, lwd = 3, lty = 3)
+lines(SE_l, lwd = 3, lty = 3)
+lines(W_mean,  lwd = 3)
+
+# plot 2
+days <- seq(0,100, by=2)
+cov_W <- var.fd(W.obj)
+var_mat <-  eval.bifd(days,days,cov_W)
+
+fig <- plot_ly(x = days, y = days, z = ~var_mat) %>% 
+  add_surface(contours = list(
+    z = list(show=TRUE,usecolormap=TRUE, highlightcolor="#ff0000", project=list(z=TRUE))))
+fig <- fig %>% 
+  layout(scene = list(camera=list(eye = list(x=1.87, y=0.88, z=-0.64))))
+fig #shows how covariance varies at different points in time
+
+
+# fPCA
+fun_pca <- pca.fd(W.obj, nharm = 4) #First four principal components
+plot(fun_pca$harmonics, lwd = 3)
+fun_pca$values #the complete set of eigenvalues
+fun_pca$varprop #a vector giving the proportion of variance explained by each eigenfunction
+sum(fun_pca$varprop) #explains 89% of the variance
+fun_pca$scores[1:4,] #a matrix of scores on the principal components or harmonics, there are 15967 values, one for each curve
+
+
+##------------------------------ AP_GRF_Data
+fda_AP_tibble <- as_tibble(plot_AP_GRF)
+
+n_points <- 100
+n_curves <- 15697 #number of observations
+max_time <- 100
+
+# setting knots & plotting basis
+knots = c(seq(0, max_time, 5)) #Location of knots
+n_knots = length(knots) #Number of knots
+n_order = 4 # order of basis functions: for cubic b-splines: order = 3 + 1
+n_basis   = length(knots) + n_order - 2;
+basis = create.bspline.basis(rangeval = c(0, max_time), n_basis)
+
+plot(basis) #currently using 20 knots, could try another number of knots to see if we obtain better variance
+
+# creating matrix for Time & value
+argvals <- matrix(fda_AP_tibble$Time, nrow = n_points, ncol = n_curves)
+y_mat <- matrix(fda_AP_tibble$value, nrow = n_points, ncol = n_curves)
+
+# creating object
+W.obj <- Data2fd(argvals = argvals, y = y_mat, basisobj = basis, lambda = 0.5)
+
+# finding mean & sd
+W_mean <- mean.fd(W.obj)
+W_sd <- std.fd(W.obj)
+
+# finding upper and lower SE
+SE_u <- fd(basisobj = basis)
+SE_l <- fd(basisobj = basis)
+
+# inputting SE's
+SE_u$coefs <- W_mean$coefs +  1.96 * W_sd$coefs/sqrt(n_curves) 
+SE_l$coefs <- W_mean$coefs -  1.96 * W_sd$coefs/sqrt(n_curves)
+
+# plot 1
+plot(W.obj, xlab="Time", ylab="", lty = 1)
+
+title(main = "Smoothed Curves")
+lines(SE_u, lwd = 3, lty = 3)
+lines(SE_l, lwd = 3, lty = 3)
+lines(W_mean,  lwd = 3)
+
+# plot 2
+days <- seq(0,100, by=2)
+cov_W <- var.fd(W.obj)
+var_mat <-  eval.bifd(days,days,cov_W)
+
+fig <- plot_ly(x = days, y = days, z = ~var_mat) %>% 
+  add_surface(contours = list(
+    z = list(show=TRUE,usecolormap=TRUE, highlightcolor="#ff0000", project=list(z=TRUE))))
+fig <- fig %>% 
+  layout(scene = list(camera=list(eye = list(x=1.87, y=0.88, z=-0.64))))
+fig #shows how covariance varies at different points in time
+
+
+# fPCA
+fun_pca <- pca.fd(W.obj, nharm = 4) #First four principal components
+plot(fun_pca$harmonics, lwd = 3)
+fun_pca$values #the complete set of eigenvalues
+fun_pca$varprop #a vector giving the proportion of variance explained by each eigenfunction
+sum(fun_pca$varprop) #explains 89% of the variance
+fun_pca$scores[1:4,] #a matrix of scores on the principal components or harmonics, there are 15967 values, one for each curve
